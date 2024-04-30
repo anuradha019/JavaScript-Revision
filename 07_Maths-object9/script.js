8b1b686db046774761929aa18fe6493634088d8f
anuradha-022 <annumangrati22@gmail.com>
2024-04-30 21:25:49 +0530

    learn truthy and falsy values boolean

---
+++ 07_Maths-object9/script.js
@@ -0,0 +1,5 @@
+const width = +prompt("Please enter rectangle Width")
+const height = +prompt("Please enter rectangle Height")
+
+console.log(width * height);
+document.write(`Area of rectangle : ${width * height}`);
