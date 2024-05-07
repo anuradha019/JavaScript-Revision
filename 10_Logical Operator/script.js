92a4ab874d74a6f22921b7968602981dfb4878b5
anuradha-022 <annumangrati22@gmail.com>
2024-05-07 20:52:41 +0530

    complete operators

--- 10_Logical Operator/script.js
+++ 10_Logical Operator/script.js
@@ -2,3 +2,19 @@ const userAge = 15
 
 const isCollegeStudent =  (userAge >= 18) && (userAge <= 24)
 const isSchoolStudent = (userAge >= 5) && (userAge <= 18)
+
+const isStudent = isSchoolStudent || isCollegeStudent
+
+const andResult = 0 && 2
+const orResult = 0 || 2
+
+const andResult2 = 'Hello' && null
+const orResult2 = 'hello' || null
+
+const andResult3 = 'hello' && -4 + 4
+const orResult3 = undefined || 4 + 8 * 5
+
+const andResult4 = 'hello' && console.log('hello')
+const orResult4 = undefined || console.log('hello')
+
+// const logResult = console.log('hello')
