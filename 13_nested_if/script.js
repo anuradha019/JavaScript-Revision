a7e408ea716e4ea63c41e8f50bd2775978926101
anuradha-022 <annumangrati22@gmail.com>
2024-05-18 21:21:08 +0530

    nested  if

---
+++ 13_nested_if/script.js
@@ -0,0 +1,23 @@
+const username = prompt('Please enter your Name');
+const userAge = parseInt(prompt('Enter your Age'));
+
+console.log(`Name : ${username}`);
+console.log(`Age : ${userAge}`);
+ 
+if(userAge >= 0 && userAge <= 4) {
+    console.log(`${username}  is  a Kid`)
+} else if(userAge >= 5 && userAge <= 17) {
+    console.log(`${username}  is  School Student`)
+}else if(userAge >= 18 && userAge <= 24) {
+    console.log(`${username}  is Student`)
+}else if (userAge >= 25 && userAge <= 59) {
+    console.log(`${username}  is working proffesional`)
+}else if (userAge >= 60 && userAge <= 120) {
+    console.log(`${username} is Senior citizen`)
+}else if (userAge >= 121) {
+    console.log(`${username} is now mortal`)
+} else { 
+    console.log('Enter valid age')
+} 
+console.log('Program Ended!!');
+   
